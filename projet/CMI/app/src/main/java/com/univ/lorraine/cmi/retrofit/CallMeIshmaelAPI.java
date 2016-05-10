ae49a2f9a4a6568ec0b437fb6986452a588e3f63
Alexis <ale.lanoix@gmail.com>
2016-05-10 12:25:03 +0200

    Base retrofit

---
+++ projet/CMI/app/src/main/java/com/univ/lorraine/cmi/retrofit/CallMeIshmaelAPI.java
@@ -0,0 +1,11 @@
+package com.univ.lorraine.cmi.retrofit;
+
+import retrofit2.http.GET;
+
+/**
+ * Interface de l'API REST pour Retrofit
+ */
+public interface CallMeIshmaelAPI {
+    // Exemple
+    //@GET("/livre/{livre}")
+}
