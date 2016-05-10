7ba98a11f30622687b7e750684d41383ad076577
Alexis <ale.lanoix@gmail.com>
2016-05-10 14:05:50 +0200

    Base retrofit +

--- projet/CMI/app/src/main/java/com/univ/lorraine/cmi/retrofit/CallMeIshmaelAPI.java
+++ projet/CMI/app/src/main/java/com/univ/lorraine/cmi/retrofit/CallMeIshmaelAPI.java
@@ -7,5 +7,7 @@ import retrofit2.http.GET;
  */
 public interface CallMeIshmaelAPI {
     // Exemple
-    //@GET("/livre/{livre}")
+    //@GET("/livre/{id}")
+    //Call<Livre> getLivre(@Path("id") String id);
+    // @Path("id") String id signifie que {id} dans l'url sera remplacé par l'id dans la méthode
 }
