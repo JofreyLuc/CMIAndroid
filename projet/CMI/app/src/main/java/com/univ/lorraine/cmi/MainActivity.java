4ce5f377ee86c87bef479ad9c39a8c9c8a47af4a
Alexis <ale.lanoix@gmail.com>
2016-05-10 19:04:01 +0200

    Ajout librairie FilePicker

--- projet/CMI/app/src/main/java/com/univ/lorraine/cmi/MainActivity.java
+++ projet/CMI/app/src/main/java/com/univ/lorraine/cmi/MainActivity.java
@@ -1,6 +1,8 @@
 package com.univ.lorraine.cmi;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import android.widget.Toast;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
+import com.nononsenseapps.filepicker.FilePickerActivity;
 import com.univ.lorraine.cmi.database.CmidbaOpenDatabaseHelper;
 import com.univ.lorraine.cmi.database.model.Livre;
 
@@ -78,6 +81,28 @@ public class MainActivity extends AppCompatActivity {
         }
         // Création du dossier interne de l'app
         getApplicationContext().getDir("CallMeIshmael", Context.MODE_PRIVATE);
+
+       testFilePicker();
+    }
+
+    private void testFilePicker() {
+        // This always works
+        Intent i = new Intent(getApplicationContext(), FilePickerActivity.class);
+        // This works if you defined the intent filter
+        // Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+
+        // Set these depending on your use case. These are the defaults.
+        i.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+        i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
+        i.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+
+        // Configure initial directory by specifying a String.
+        // You could specify a String like "/storage/emulated/0/", but that can
+        // dangerous. Always use Android's API calls to get paths to the SD-card or
+        // internal memory.
+        i.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
+
+        startActivityForResult(i, 1);
     }
 
     private void testDatabase() throws SQLException{
