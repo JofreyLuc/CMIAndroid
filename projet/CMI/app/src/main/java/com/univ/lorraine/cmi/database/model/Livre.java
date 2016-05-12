9540449279e150fbc2fa3b477831162bb582a615
Alexis <ale.lanoix@gmail.com>
2016-05-12 11:06:45 +0200

    Changement des méthodes d'importation de livre + tabke livre dans bdd

--- projet/CMI/app/src/main/java/com/univ/lorraine/cmi/database/model/Livre.java
+++ projet/CMI/app/src/main/java/com/univ/lorraine/cmi/database/model/Livre.java
@@ -5,6 +5,10 @@ import com.j256.ormlite.table.DatabaseTable;
 
 import java.util.Date;
 
+import nl.siegmann.epublib.domain.Author;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Metadata;
+
 /**
  * Created by jyeil on 10/05/16.
  */
@@ -45,9 +49,6 @@ public class Livre {
     @DatabaseField
     private String lienCouverture;
 
-    @DatabaseField
-    private String cheminFichier;
-
     // Needed by ORMlite
     Livre() {}
 
@@ -61,6 +62,74 @@ public class Livre {
         noteMoyenne = noteM;
         lienDLEpub = lienDL;
         lienCouverture = lienCou;
+    }
+
+    /**
+     * Crée un livre à partir d'un objet book en extrayant ses metadatas.
+     *
+     * @param book Objet book.
+     */
+    public Livre(Book book) {
+        Metadata meta = book.getMetadata();
+
+        // Titres
+        StringBuilder titres = new StringBuilder();
+        String delim = "";
+        for (String t : meta.getTitles()) {
+            titres.append(delim);
+            titres.append(t);
+            delim = ", ";
+        }
+        this.titre = titres.toString();
+
+        // Auteurs
+        delim = "";
+        StringBuilder auteurs = new StringBuilder();
+        for (Author a : meta.getAuthors()) {
+            titres.append(delim);
+            if (a.getFirstname() != null) {
+                auteurs.append(a.getFirstname() + " ");
+            }
+            if (a.getLastname() != null) {
+                auteurs.append(a.getLastname());
+            }
+            delim = ", ";
+        }
+        this.auteur = auteurs.toString();
+
+        // Genre
+        delim = "";
+        StringBuilder types = new StringBuilder();
+        for (String g : meta.getTypes()) {
+            titres.append(delim);
+            types.append(g);
+            delim = ", ";
+        }
+        this.genre = types.toString();
+
+        // Dates
+        nl.siegmann.epublib.domain.Date date = null;
+        String dateString;
+        for (nl.siegmann.epublib.domain.Date d : meta.getDates()) {
+            if (d.getEvent() == nl.siegmann.epublib.domain.Date.Event.PUBLICATION) date = d;
+        }
+        if (date == null) dateString = "";
+        else dateString = date.toString();
+        this.dateParution = dateString;
+
+        // Résumé
+        delim = "";
+        StringBuilder resumes = new StringBuilder();
+        for (String r : meta.getDescriptions()) {
+            titres.append(delim);
+            resumes.append(r);
+            delim = ", ";
+        }
+        this.resume = resumes.toString();
+    }
+
+    public Long getIdLivre() {
+        return idLivre;
     }
 
     @Override
