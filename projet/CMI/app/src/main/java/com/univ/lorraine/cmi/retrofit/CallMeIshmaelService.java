ed13c4b617f844072ae62928ffb1eddd61878a6d
Alexis <ale.lanoix@gmail.com>
2016-05-18 16:14:44 +0200

    Ajout méthodes api retrofit

--- projet/CMI/app/src/main/java/com/univ/lorraine/cmi/retrofit/CallMeIshmaelService.java
+++ projet/CMI/app/src/main/java/com/univ/lorraine/cmi/retrofit/CallMeIshmaelService.java
@@ -1,14 +1,68 @@
 package com.univ.lorraine.cmi.retrofit;
 
+import com.univ.lorraine.cmi.database.model.Annotation;
+import com.univ.lorraine.cmi.database.model.Bibliotheque;
+import com.univ.lorraine.cmi.database.model.Livre;
+import com.univ.lorraine.cmi.database.model.Utilisateur;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Interface Retrofit pour l'API REST CallMeIshmael.
  */
 public interface CallMeIshmaelService {
-    // Exemple
-    //@GET("/livre/{id}")
-    //Call<Livre> getLivre(@Path("id") String id);
-    // @Path("id") String id signifie que {id} dans l'url sera remplacé par l'id dans la méthode
+
+    // Utilisateur
+
+    @GET("users/{idUser}")
+    Call<Utilisateur> getUtilisateur(@Path("idUser") Long idUtilisateur);
+
+    @PUT("users")
+    Call<ResponseBody> updateUtilisateur(@Body Utilisateur utilisateur);
+
+    // Livre
+
+    @GET("books/{idBook}")
+    Call<Livre> getLivre(@Path("idBook") Long idLivre);
+
+    // Recherche de livres
+    @GET("books")
+    Call<List<Livre>> searchLivre(@Query("titre") String titre, @Query("author") String auteur, @Query("language") String langue, @Query("start-limit") Integer startLimit, @Query("end-limit") Integer endLimit);
+
+    // Bibliotheque
+
+    @GET("users/{idUser}/library")
+    Call<List<Bibliotheque>> getBibliotheques(@Path("idUser") Long idUtilisateur);
+
+    @GET("users/{idUser}/library/{idLibrary}")
+    Call<List<Bibliotheque>> getBibliotheque(@Path("idUser") Long idUtilisateur, @Path("idLibrary") Long idBibliotheque);
+
+    @POST("users/{idUser}/library")
+    Call<Bibliotheque> createBibliotheque(@Path("idUser") Long idUtilisateur, @Body Bibliotheque bibliotheque);
+
+    @PUT("users/{idUser}/library")
+    Call<ResponseBody> updateBibliotheque(@Path("idUser") Long idUtilisateur, @Body Bibliotheque bibliotheque);
+
+    // Notes
+    @GET("users/{idUser}/library/{idLibrary}/notes")
+    Call<List<Annotation>> getAnnotations(@Path("idUser") Long idUtilisateur, @Path("idLibrary") Long idBibliotheque);
+
+    @GET("users/{idUser}/library/{idLibrary}/notes/{idNote}")
+    Call<Annotation> getAnnotation(@Path("idUser") Long idUtilisateur, @Path("idLibrary") Long idBibliotheque, @Path("idNote") Long idAnnotation);
+
+    @POST("users/{idUser}/library/{idLibrary}/notes")
+    Call<Annotation> createAnnotation(@Path("idUser") Long idUtilisateur, @Path("idLibrary") Long idBibliotheque, @Body Annotation annotation);
+
+    @PUT("users/{idUser}/library/{idLibrary}/notes")
+    Call<ResponseBody> updateAnnotation(@Path("idUser") Long idUtilisateur, @Path("idLibrary") Long idBibliotheque, @Body Annotation annotation);
 
 }
