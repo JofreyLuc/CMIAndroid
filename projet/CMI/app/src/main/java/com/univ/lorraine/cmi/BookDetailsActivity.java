01d472e7affc5bb01c1fc2e21ea755a1c0c040e5
Jyeil <jyeil9@laposte.net>
2016-05-30 09:26:42 +0200

    Modif couleur bouton (truc de fou)

--- projet/CMI/app/src/main/java/com/univ/lorraine/cmi/BookDetailsActivity.java
+++ projet/CMI/app/src/main/java/com/univ/lorraine/cmi/BookDetailsActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -271,7 +272,7 @@ public class BookDetailsActivity extends AppCompatActivity {
 
     private void disableBoutonAjout() {
         boutonAjout.setEnabled(false);
-        boutonAjout.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
+        boutonAjout.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorBordersDark));
         boutonAjout.setText(R.string.button_add_inactive);
         boutonLecture.setText(R.string.button_readNow_alt);
     }
